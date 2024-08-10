7f40046451887293572ae52f20d14193f6f8ce57
faysaltaysir <faysaltaysir1435@gmail.com>
2024-08-10 22:32:25 +0600

    Add .gitattributes for consistent line endings

--- src/components/OtpView.vue
+++ src/components/OtpView.vue
@@ -2,34 +2,35 @@
     <div class="inputs">
         <div class="bind">
             <div class="text-div">
-                    <p class="txt">OTP Fillup</p>
-                </div>
+                <p class="txt">OTP Fillup</p>
+            </div>
             <div class="otp-boxs">
-                
+
                 <input type="text" class="box">
                 <input type="text" class="box">
                 <p class="otp-hipen">-</p>
                 <input type="text" class="box">
-                <input type="text" class="box">       
+                <input type="text" class="box">
             </div>
         </div>
-            
+
     </div>
-    
+
 </template>
 <style scoped>
-.bind{
+.bind {
     width: 100%;
     height: 85px;
     /* height: 100%; */
     /* background-color: blue; */
 }
-.text-div{
-    padding: 0px 0px 10px 3px ;
+
+.text-div {
+    padding: 0px 0px 10px 3px;
     display: flex;
 }
 
-.inputs{
+.inputs {
     width: 100%;
     height: max-content;
     color: #433E3F;
@@ -41,35 +42,39 @@
     margin-top: 100px;
     margin-bottom: 100px;
 }
-.otp-hipen{
+
+.otp-hipen {
     font-size: 1.5rem;
 }
-    .otp-boxs{
-        width: 100%;
-        height: max-content;
-        display: flex;
-        /* width: 100%; */
-        /* background-color: rgb(207, 9, 9); */
-        justify-content: space-between;
-        align-items: center;
-        /* padding: 0px 2vw; */
-    /* background-color: aqua; */
-    }
-    .box{
-        border-radius: 5px;
-        width: 21%;
-        height: 55px;
-        max-width: 100px;
-        border: 1px solid #D9D9D9;
 
-    }
-    @media (max-width: 700px) {
-        .otp-boxs{
-            /* width: 80%; */
-            /* background-color: red; */
+.otp-boxs {
+    width: 100%;
+    height: max-content;
+    display: flex;
+    /* width: 100%; */
+    /* background-color: rgb(207, 9, 9); */
+    justify-content: space-between;
+    align-items: center;
+    /* padding: 0px 2vw; */
+    /* background-color: aqua; */
+}
+
+.box {
+    border-radius: 5px;
+    width: 21%;
+    height: 55px;
+    max-width: 100px;
+    border: 1px solid #D9D9D9;
+
+}
+
+@media (max-width: 700px) {
+    /* .otp-boxs{
+            width: 80%;
+            background-color: red;
         }
         .box{
-            /* width: 80px; */
-        }
-    }
+            width: 80px;
+        } */
+}
 </style>
